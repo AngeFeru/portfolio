7d149be2a42876b0e8170cb0cc7e2734502cb98c
Angelo Ferulano <angelofr08@gmail.com>
2024-10-24 20:21:03 -0300

    Finish

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import Projects from '@/components/projects'
 import Skills from '@/components/skills'
 import Experience from '@/components/experience'
 import Contact from '@/components/contact'
-import ReactGA from 'react-ga4';
+
 
 
 export default function Home() {
-  ReactGA.initialize('G-DHL39DMW3S');
+ 
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
